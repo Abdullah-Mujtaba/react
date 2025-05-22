55b628fc31fecef1c33e0f2b39d1aeaf98602d53
Abdullah-Mujtaba <ahmedmujtaba823@gmail.com>
2025-05-23 04:39:06 +0500

    user cards now working

--- React-project/src/components/UserCard.jsx
+++ React-project/src/components/UserCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import userImage from '../assets/images.jpeg';
+import "./card.css"
 
 const UserCard = () => {
   return (
     <div className='user-container'>
       <p id='title'>hi my name is</p>
-      <img id='user-img' src="" alt='love'></img>
+      <img id='user-img' src={userImage} alt='love'></img>
       <p id='user-description'> Description of user</p>
     </div>
   )
